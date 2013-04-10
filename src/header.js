70a560d6bbc06fe6216ce59f6e1b956df91c3f0c
Peter Michaux <petermichaux@gmail.com>
2013-04-10 16:46:34 -0700

    increment version

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 /*
-Hormigas version 4
-Copyright (c) 2012, Peter Michaux
+Hormigas version 5
+Copyright (c) 2013, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
 https://github.com/petermichaux/hormigas/blob/master/LICENSE
